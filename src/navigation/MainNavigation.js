adfa8939405dbe1261e62f78bedd5be3393eb460
Lucas <jawsandeur@gmail.com>
2022-05-25 21:46:08 +0800

    Added bottom navigation bar and home screen

---
+++ src/navigation/MainNavigation.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import { NavigationContainer, TabActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { MaterialIcons } from "@expo/vector-icons";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { auth } from "../firebase";
+import { Login, Track, AddItem, Register } from "../screens";
+import HomeScreen from "../screens/HomeScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const homeName = "Home";
+const trackName = "Track";
+const settingsName = "Settings";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const Stack2 = createNativeStackNavigator();
+const TrackStack = createNativeStackNavigator();
+
+const MainNavigation = () => {
+    /**
+	 * This hook serves as a listener to auth state changes provided by firebase.
+	 */
+	const [isAuth, setIsAuth] = useState(false);
+
+	useEffect(() => {
+		const unsubscribeAuthStateChanged = onAuthStateChanged(
+			auth,
+			(authenticatedUser) => {
+				if (authenticatedUser) {
+					setIsAuth(true);
+				} else {
+					setIsAuth(false);
+				}
+			}
+		);
+
+		return unsubscribeAuthStateChanged;
+	}, []);
+
+    const HomeStack = () => (
+        <Stack2.Navigator> 
+            <Stack2.Screen name={homeName} component={HomeScreen} />
+        </Stack2.Navigator>
+    );
+    
+    const SettingsStack = () => (
+        <Stack2.Navigator> 
+            <Stack2.Screen name={settingsName} component={SettingsScreen} options={{ headerRight: () => <LogoutIcon /> }}/>
+        </Stack2.Navigator>
+    );
+
+	const LoginNavigator = () => (
+		<Stack.Navigator initialRouteName={Login}>
+			<Stack.Screen
+				name="Login"
+				component={Login}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Register"
+				component={Register}
+				options={{ headerShown: false }}
+			/>
+		</Stack.Navigator>
+	);
+
+	const logoutHandler = () => {
+		signOut(auth).then(() => {
+			setIsAuth(false);
+			setUser({});
+		});
+	};
+
+	const LogoutIcon = () => (
+		<TouchableOpacity onPress={logoutHandler}>
+			<MaterialIcons name="logout" size={28} color="#407BFF" />
+		</TouchableOpacity>
+	); 
+
+    const TrackNavigator = () => (
+        <TrackStack.Navigator initialRouteName="Currently Tracking">
+            <TrackStack.Screen
+                name="Currently Tracking"
+                component={Track}
+            />
+            <TrackStack.Screen name="Add Item" component={AddItem} />
+        </TrackStack.Navigator>
+    );
+
+    const MainNavigator = () => (
+        <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                let currentTab = route.name;
+
+                if (currentTab === homeName) {
+                    iconName = focused ? 'home' : 'home-outline';
+                } else if (currentTab === trackName) {
+                    iconName = focused ? 'list' : 'list-outline';
+                } else if (currentTab === settingsName) {
+                    iconName = focused ? 'settings' : 'settings-outline';
+                }
+
+                return <Ionicons name={iconName} size={size} color={color}/>
+            },
+        })}>
+
+        <Tab.Screen name={homeName} component={HomeStack} options={{ headerShown: false}}/>
+        <Tab.Screen name={trackName} component={TrackNavigator} options={{ headerShown: false}}/>
+        <Tab.Screen name={settingsName} component={SettingsStack} options={{ headerShown: false}}/>
+
+        </Tab.Navigator>
+    );
+
+    return (
+        <NavigationContainer>
+            {isAuth ? <MainNavigator /> : <LoginNavigator />}
+        </NavigationContainer> 
+    )
+};
+
+export default MainNavigation;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/Searchbar/SearchBar";
+
+const HomeScreen = ({ navigation }) => {
+    const [searchPhrase, setSearchPhrase] = useState('');
+    const [clicked, setClicked] = useState(false);
+
+    return (
+        <SearchBar 
+            searchPhrase={searchPhrase} 
+            setSearchPhrase={setSearchPhrase}
+            clicked={clicked}
+            setClicked={setClicked}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    home: {
+        flex: 1,
+        justifyContent: 'flex-top',
+        alignItems: 'center',
+    }
+});
+
+export default HomeScreen;
